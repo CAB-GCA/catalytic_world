5cf40f9230b1ed946d9c3c4bce6549bf26f4c784
leope8 <cvzads08@gmail.com>
2025-10-10 12:51:13 +0200

    cambio bucle gillespie

--- leo_Gilles.py
+++ leo_Gilles.py
@@ -4,13 +4,13 @@ from fun_gilles import *
 
 # Initialization:
 file = "reactions_XYC.txt" # M reactions
-n_iterations = 210
+n_iterations = 10000
 method = "Gillespie" # Gillespie or Deterministic
 # Reaction constants:
-k = [1,.01]*2 # len(k)= # de reacciones
+k = [10,1,10,1] # len(k)= # de reacciones
 # Volume:
-V = 0.1
-initial_food = [120,100,120] # initial molecules number
+V = 100
+initial_food = 1000 # initial molecules number
 food_molecules = 3
 
 reactions = read_file(file)
@@ -20,20 +20,18 @@ species = obtain_species(reactions)
 abundances, times = chemistry(method, n_iterations, reactions, food_molecules, 
                               initial_food, k, V)
 
-it= n_iterations
 # Representation
 plt.figure()
 colors = plt.cm.coolwarm(np.linspace(0, 1, len(species)))
 plt.grid()
 
 for i in range(len(species)):
-    plt.plot(times[:it], abundances[:it, i], label=species[i], color=colors[i], alpha=0.8)
+    plt.plot(times, abundances[:, i], label=species[i], color=colors[i], alpha=0.9)
 
 plt.grid(True, linestyle='--', alpha=0.3)
 plt.xlabel("Time")
 plt.ylabel("Abundances")
 plt.legend()
-plt.title(f"{method} time evolution. V={V}")
-
+plt.title(f"{method} time evolution: {n_iterations} iterations, k_a= {k[0]}")
     
 plt.show()
